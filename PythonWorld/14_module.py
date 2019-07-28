8b1be39ac55a6038585b3ba7cf31ecb418004e87
kollivalasaNagaraju <nagarajubasara@gmail.com>
2019-07-28 23:54:37 +0530

    Create 14_module.py

---
+++ PythonWorld/14_module.py
@@ -0,0 +1,12 @@
+import moduleCreate
+moduleCreate.pythonModule1()
+moduleCreate.pythonModule2()
+print(moduleCreate.variableName)
+
+
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
